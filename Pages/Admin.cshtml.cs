877ef87eb12c95f1e748228a85f0eaf462b13938
Jackson Rakena <jackson@gocloud.systems>
2022-03-27 17:19:23 +1300

    Fix bypass bug

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Awch.Site.Pages;
 
 public class Admin : PageModel
 {
-    public void OnGet()
+    public IActionResult OnGet()
     {
-        if (!User.IsInRole("Administrator")) RedirectToPage("/Login");
+        if (!User.IsInRole("Administrator")) return RedirectToPage("/Login");
+        return Page();
     }
 }
