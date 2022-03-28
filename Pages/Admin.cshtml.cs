fd69ca27f533c22e7f3a2cccf18fe5ce90dabe1b
Jackson Rakena <jackson@gocloud.systems>
2022-03-28 18:41:43 +1300

    Easy database updates

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Awch.Site.Pages;
 
@@ -18,4 +21,45 @@ public class Admin : PageModel
         if (!User.IsInRole("Administrator")) return RedirectToPage("/Login");
         return Page();
     }
+    
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    public class InputModel
+    {
+        [Required]
+        public string Url { get; set; }
+        
+        [Required]
+        public string Submitter { get; set; }
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid) return Page();
+        
+        try
+        {
+            if (await Context.ImageRecords.AnyAsync(e => e.Url == Input.Url))
+            {
+                ModelState.AddModelError("Url", "This is already in the database.");
+                return Page();
+            }
+            Context.ImageRecords.Add(new ImageRecord
+            {
+                Author = Input.Submitter,
+                Url = Input.Url
+            });
+            await Context.SaveChangesAsync();
+            ModelState.Clear();
+        }
+        catch (Exception e)
+        {
+            ModelState.AddModelError("db", e.Message);
+        }
+
+        // If we got this far, something failed, redisplay form
+        return Page();
+    }
+    
 }
